610eaee965bfa645860fc5a1b3dfb4f300059f63
Puffer Notebook <marceloballinflores@gmail.com>
2025-08-04 19:08:05 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraAplicacaoDotCoreWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraAplicacaoDotCoreWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NovoUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult PersistirUsuario(string nome, string sobrenome, string email)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Servico/Db.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Servico
+{
+    public class Db
+    {
+        private readonly MySqlConnection _con;
+        private MySqlCommand? _command;
+        private MySqlDataReader? _reader;
+
+        public Db()
+        {
+            _con = new MySqlConnection("Server=127.0.0.1;Database=cadusuario;Uid=root;Pwd=SslMode=none;")
+        }
+
+        public List<UsuarioDTO> GetData()
+        {
+            //para parar de criticar o GetData, coloquei o retorno de uma lista vazia -->  return [];
+            _con.Open();
+            _command = new MySqlCommand();
+            _command.Connection = _con;
+            _command.CommandText = "SELECT * FROM usuario";
+            _reader = _command.ExecuteReader();
+
+            List<UsuarioDTO> lisdaUsuarios = new List<UsuarioDTO>();
+
+            while (_reader.Read())
+            {
+                var UsuarioDTO = new UsuarioDTO
+                {
+                    id = int.Parse(_reader["Id"].ToString()!),
+                    nome = _reader["Nome"].ToString()!,
+                    sobrenome = _reader["Sobrenome"].ToString()!,
+                    email = _reader["Email"].ToString()!
+                };
+                lisdaUsuarios.Add(UsuarioDTO);
+            }
+
+            return lisdaUsuarios;
+        }
+
+        public void AddUsuario(string nome)
+        {
+            _con.Open();
+            _command = new MySqlCommand();
+            _command.Connection = _con;
+
+            _command.CommandText = "INSERT INTO usuario (nome, sobrenome, email) VALUES (?nome, ?sobrenome, ?email)";
+            _command.Parameters.Add("?nome", MySqlDbType.String).Value = nome;
+        }
+    }
+}
